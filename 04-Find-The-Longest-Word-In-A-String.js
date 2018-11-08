b0204456fc521afeeb3cb9952df00ae923e596b3
Steven Ang <spikegreen2@gmail.com>
2018-11-08 22:07:41 +0800

    Update 04-Find-The-Longest-Word-In-A-String.js

--- 04-Find-The-Longest-Word-In-A-String.js
+++ 04-Find-The-Longest-Word-In-A-String.js
@@ -1,8 +1,16 @@
+// Solution 1
 function findLongestWord(str) {
   return str.split(' ')
     .reduce(function(a, b) {
       return Math.max(a, b.length)
     }, 0);
+}
+
+// Solution 2
+function findLongestWordLength(str) {
+  return str
+    .split(" ")
+    .sort((a, b) => b.length - a.length )[0].length;
 }
 
 console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
