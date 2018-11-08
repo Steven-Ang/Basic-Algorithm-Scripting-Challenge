2914ae515a6512d007b97cf6e8e1e6c856e073ea
Steven Ang <spikegreen2@gmail.com>
2018-11-08 19:19:04 +0800

    Update 08-Repeat-A-String-Repeat-A-String.js

--- 08-Repeat-A-String-Repeat-A-String.js
+++ 08-Repeat-A-String-Repeat-A-String.js
@@ -1,8 +1,16 @@
+// Solution 1 (Cheated)
 function repeatStringNumTimes(str, num) {
-  if (num <= 0) {
-    return "";
+  return (num <= 0) ? '' : str.repeat(num);
+}
+
+// Solution 2
+function repeatStringNumTimes(str, num) {
+  if (num <= 0) return '';
+  let result = '';
+  for (let i = 0; i < num; i++) {
+    result = str + result;
   }
-  return str.repeat(num);
+  return result;
 }
 
 repeatStringNumTimes("abc", 3);
