1bd35f4dbc0c6a7190717b82f39a1061f8840386
Steven Ang <spikegreen2@gmail.com>
2018-11-08 20:24:20 +0800

    Update 02-Factorialize-A-Number.js

--- 02-Factorialize-A-Number.js
+++ 02-Factorialize-A-Number.js
@@ -1,3 +1,4 @@
+// Solution 1
 function factorialize(num) {
     var count = 1;
     for (var i = 1; i <= num ; i++) {
@@ -6,12 +7,9 @@ function factorialize(num) {
     return count;
 }
 
-// Test Cases
-console.log("\nTest Case 1: \nInput: 5 \nExpected Output: 120");
-console.log("Output: " + factorialize(5) + "\n");
+// Solution 2
+function factorialize(num) {
+  return (num === 0) ? 1 : num * factorialize(num - 1);
+}
 
-console.log("Test Case 2: \nInput: 20 \nExpected Output: 2432902008176640000");
-console.log("Output: " + factorialize(20) + "\n");
-
-console.log("Test Case 2: \nInput: 200 \nExpected Output: Infinity");
-console.log("Output: " + factorialize(200));
+factorialize(5);
