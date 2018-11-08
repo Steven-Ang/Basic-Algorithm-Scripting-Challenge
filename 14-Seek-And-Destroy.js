6bb9a3c10e611b26737f222e4c6a7b1e8b7889d7
Steven Ang <spikegreen2@gmail.com>
2018-11-09 03:45:49 +0800

    Update 14-Seek-And-Destroy.js

--- 14-Seek-And-Destroy.js
+++ 14-Seek-And-Destroy.js
@@ -1,12 +1,13 @@
 function destroyer(arr) {
-  var args = Array.from(arguments);
-
-  for (var i = 1; i < args.length; i++) {
-    args[0] = args[0].filter(function(val) {
-      return val !== args[i];
-    });
-  }
-
+	// Get all of the arguments and turn them into an array
+  let args = Array.from(arguments);
+	// args[0] represents as the given array
+  for (let i = 1; i < args.length; i++) {
+		// Filter out the array
+		// Remove any element that matches the arguments given
+		args[0] = args[0].filter(val => val !== args[i])
+	}
+	// Return the modified array
   return args[0];
 }
 
