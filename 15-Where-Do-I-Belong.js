e4d1bcb00d3fa7c2f828e211a67f3b7c3cf7a4e2
Steven Ang <spikegreen2@gmail.com>
2018-11-08 21:34:13 +0800

    Update 15-Where-Do-I-Belong.js

--- 15-Where-Do-I-Belong.js
+++ 15-Where-Do-I-Belong.js
@@ -1,3 +1,5 @@
+// Old solution
+// Future me: You could have shorten it.
 function getIndexToIns(arr, num) {
   // Find my place in this sorted array.
   var results = arr;
@@ -6,6 +8,15 @@ function getIndexToIns(arr, num) {
     return a - b;
   });
   return results.indexOf(num);
+}
+
+// New Soluton
+function getIndexToIns(arr, num) {
+  // Push the item to the array
+  arr.push(num)
+  return arr
+  .sort((a, b) => a - b) // Sort the array in ASC order
+  .indexOf(num); // Find the position of the given number and return its index.
 }
 
 getIndexToIns([40, 60], 50);
