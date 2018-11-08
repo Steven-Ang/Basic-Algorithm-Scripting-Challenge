f46b86450916451857f87f35a4a819948d49067a
Steven Ang <spikegreen2@gmail.com>
2018-11-08 21:28:07 +0800

    Update 13-Falsy-Bouncer.js

--- 13-Falsy-Bouncer.js
+++ 13-Falsy-Bouncer.js
@@ -1,8 +1,6 @@
 function bouncer(arr) {
-  // Don't show a false ID to this bouncer.
-  return arr.filter(function(val) {
-    return val;
-  });
+  // Only retrieve the truly items. (false, undefined, 0, '', null, NaN) will be excluded.
+  return arr.filter(val => val);
 }
 
 bouncer([7, "ate", "", false, 9]);
