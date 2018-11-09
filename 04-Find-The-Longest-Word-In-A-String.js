8ade1273df94615c8c8a4a905ff7722df9ca8139
Steven Ang <spikegreen2@gmail.com>
2018-11-09 10:50:13 +0800

    Update 04-Find-The-Longest-Word-In-A-String.js

--- 04-Find-The-Longest-Word-In-A-String.js
+++ 04-Find-The-Longest-Word-In-A-String.js
@@ -13,4 +13,14 @@ function findLongestWordLength(str) {
     .sort((a, b) => b.length - a.length )[0].length;
 }
 
+// Solution 3 | Different method
+function longestWord(str) {
+  // Sort it by the longest length
+	const sorted = str.split(' ').sort((a, b) => b.length - a.length);
+  // If multiplie words, put into array
+	const results = sorted.filter(word => word.length === sorted[0].length);
+  // Return the string if there's only one longest word or else return an array
+	return (results.length === 1) ? results[0] : results;
+}
+
 console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
